558c6906c76ebfd6b9b4fe906b69b40ec693431e
manjunath <k.mnjunth@gmail.com>
2024-11-30 20:11:47 +0530

    product images added

--- data/products.js
+++ data/products.js
@@ -10,7 +10,7 @@ const products = [{
         retail_price: 129,
         sale_price: 110,
         discount_percentage: 15
-    },
+    }
 }, {
     image: "products/book2.jpg",
     name: "Parisaradha kathe",
@@ -23,7 +23,7 @@ const products = [{
         retail_price: 129,
         sale_price: 117,
         discount_percentage: 10
-    },
+    }
 }, {
     image: "products/book3.jpg",
     name: "Papillon vol(1)",
@@ -36,7 +36,7 @@ const products = [{
         retail_price: 171,
         sale_price: 154,
         discount_percentage: 10
-    },
+    }
 }, {
     image: "products/book4.jpg",
     name: "Yaana",
@@ -49,7 +49,7 @@ const products = [{
         retail_price: 275,
         sale_price: 248,
         discount_percentage: 10
-    },
+    }
 }, {
     image: "products/book5.jpg",
     name: "Wings of Fire",
@@ -62,7 +62,7 @@ const products = [{
         retail_price: 495,
         sale_price: 424,
         discount_percentage: 14
-    },
+    }
 }, {
     image: "products/book6.jpg",
     name: "VamshaVruksha",
@@ -75,7 +75,7 @@ const products = [{
         retail_price: 445,
         sale_price: 401,
         discount_percentage: 10
-    },
+    }
 }, {
     image: "products/book7.jpg",
     name: "Malegalalli Madhumagalu",
@@ -88,7 +88,7 @@ const products = [{
         retail_price: 450,
         sale_price: 383,
         discount_percentage: 15
-    },
+    }
 }, {
     image: "products/book8.jpg",
     name: "Jugari Cross",
@@ -101,5 +101,174 @@ const products = [{
         retail_price: 246,
         sale_price: 209,
         discount_percentage: 15
+    }
+}, {
+    image: "products/book9.jpg",
+    name: "Nanna Devaru matthu itara kathegalu",
+    author: "Kuvempu",
+    ratings: {
+        stars: "ratings/rating-5.png",
+        no_of_customers: 101
     },
+    price: {
+        retail_price: 80,
+        sale_price: 68,
+        discount_percentage: 15
+    }
+}, {
+    image: "products/book10.jpg",
+    name: "Bhagavadgita Yatharoopa",
+    author: "AC Bhaktivedanta Swami Prabhupada, Ls Seshagiri Rao ",
+    ratings: {
+        stars: "ratings/rating-5.png",
+        no_of_customers: 101
+    },
+    price: {
+        retail_price: 560,
+        sale_price: 476,
+        discount_percentage: 15
+    }
+}, {
+    image: "products/book11.jpg",
+    name: "Malenaadina Chitragalu",
+    author: "Kuvempu",
+    ratings: {
+        stars: "ratings/rating-5.png",
+        no_of_customers: 101
+    },
+    price: {
+        retail_price: 100,
+        sale_price: 85,
+        discount_percentage: 15
+    }
+}, {
+    image: "products/book12.jpg",
+    name: "MaariKaadu",
+    author: "Chandrashekara Kambaara",
+    ratings: {
+        stars: "ratings/rating-5.png",
+        no_of_customers: 101
+    },
+    price: {
+        retail_price: 65,
+        sale_price: 59,
+        discount_percentage: 10
+    }
+}, {
+    image: "products/book13.jpg",
+    name: "Naayi Neralu",
+    author: "SL Byrappa ",
+    ratings: {
+        stars: "ratings/rating-5.png",
+        no_of_customers: 101
+    },
+    price: {
+        retail_price: 220,
+        sale_price: 198,
+        discount_percentage: 10
+    }
+}, {
+    image: "products/book14.jpg",
+    name: "Praani Prapanchada Vismayagalu",
+    author: "K Shivarama Karanth",
+    ratings: {
+        stars: "ratings/rating-5.png",
+        no_of_customers: 101
+    },
+    price: {
+        retail_price: 65,
+        sale_price: 59,
+        discount_percentage: 10
+    }
+}, {
+    image: "products/book15.jpg",
+    name: "Chidambara Rahasya",
+    author: "KP Poornachandra Tejaswi",
+    ratings: {
+        stars: "ratings/rating-5.png",
+        no_of_customers: 101
+    },
+    price: {
+        retail_price: 246,
+        sale_price: 209,
+        discount_percentage: 15
+    }
+}, {
+    image: "products/book16.jpg",
+    name: "Bettada Jeeva",
+    author: "K Shivarama Karanth",
+    ratings: {
+        stars: "ratings/rating-5.png",
+        no_of_customers: 101
+    },
+    price: {
+        retail_price: 150,
+        sale_price: 135,
+        discount_percentage: 10
+    }
+}, {
+    image: "products/book17.jpg",
+    name: "Sanyasi matthu itara kathegalu",
+    author: "Kuvempu",
+    ratings: {
+        stars: "ratings/rating-5.png",
+        no_of_customers: 101
+    },
+    price: {
+        retail_price: 80,
+        sale_price: 68,
+        discount_percentage: 15
+    }
+}, {
+    image: "products/book18.jpg",
+    name: "Abachurina Postoffice",
+    author: "KP Poornachandra Tejaswi",
+    ratings: {
+        stars: "ratings/rating-5.png",
+        no_of_customers: 101
+    },
+    price: {
+        retail_price: 114,
+        sale_price: 97,
+        discount_percentage: 15
+    }
+}, {
+    image: "products/book19.jpg",
+    name: "Mahapalayana millenium(14)",
+    author: "KP Poornachandra Tejaswi",
+    ratings: {
+        stars: "ratings/rating-5.png",
+        no_of_customers: 101
+    },
+    price: {
+        retail_price: 108,
+        sale_price: 92,
+        discount_percentage: 15
+    }
+}, {
+    image: "products/book20.jpg",
+    name: "Maigallana dinachari",
+    author: "K Shivarama Karanth",
+    ratings: {
+        stars: "ratings/rating-5.png",
+        no_of_customers: 101
+    },
+    price: {
+        retail_price: 90,
+        sale_price: 81,
+        discount_percentage: 10
+    }
+}, {
+    image: "products/book21.jpg",
+    name: "American Prometheus : The Triumph & Tragedy Of J Robert Oppenheimer",
+    author: "Kai Bird",
+    ratings: {
+        stars: "ratings/rating-5.png",
+        no_of_customers: 101
+    },
+    price: {
+        retail_price: 899,
+        sale_price: 702,
+        discount_percentage: 22
+    }
 }]
