b88cef60b7515ca787aaaec813cf5c57806a1d6a
manjunath <k.mnjunth@gmail.com>
2024-12-04 23:44:01 +0530

    make cart quantity interactive

--- scripts/home.js
+++ scripts/home.js
@@ -60,7 +60,13 @@ document.querySelectorAll(".js-add-to-cart-button").forEach((button) => {
                 quantity: 1
             })
         }
-        console.log(cart);
+
+        let cartQuantity = 0;
+        cart.forEach((item) => {
+            cartQuantity += item.quantity;
+        })
+        const cartQuantityElement = document.querySelector(".js-cart-quantity");
+        cartQuantityElement.innerHTML = cartQuantity;
     })
 })
 
